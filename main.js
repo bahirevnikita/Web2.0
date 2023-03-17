a927f6f51127a380cf032571b63f48f8854134c9
bahirevnikita <bahirevnikita@icloud.com>
2023-03-17 17:19:32 +0300

    laba 3

--- main.js
+++ main.js
@@ -160,7 +160,7 @@
 //     number=+prompt("Номер задания")
 // }
 
-//Задача 2.1
+// Лаба 2
 function convertSpeed(x1, S)
 {
 	if(S=="toMS"){
@@ -196,12 +196,12 @@ function button_2()
 
 function object(){
     let student = {
-        group: 211-326,
+        group: "211-326",
         last_name: "Бахирев",
         first_name: "Никита"
     };
 	keys = Object.keys(student);
-	return `Cписок свйоств: ${Object.keys(student)[0]} ${Object.keys(student)[1]} ${Object.keys(student)[2]}
+	return `Cписок свойств: ${Object.keys(student)[0]} ${Object.keys(student)[1]} ${Object.keys(student)[2]}
 Студент ${student.first_name} ${student.last_name} ${student.group}`
 }
 function button_3(){
@@ -216,19 +216,99 @@ function randomNumber(min,max){
 }
 
 function getarr(){
-    let arr = new Array();
     let arr1 = new Array();
+    let arr2 = new Array();
     let n =+prompt("Введите длину массива");
     for (let i=0;i<n;i++){
-        arr[i]=prompt("Введите желаемое число");
+        arr1[i]=prompt("Введите желаемое число");
     }
     let k =+prompt("Колличество случайных значений¹");
     for (let i=0;i<k;i++){
-        arr1.push(arr[randomNumber(0,n)]);
+        arr2.push(arr1[randomNumber(0,n)]);
     }
-    return arr1;
+    return arr2;
 }
 function button_5(){
     
     console.log(getarr());
+}
+
+// Лаба 3
+const bSign = document.getElementById('sign_butt')
+const fSign = document.getElementById('sign_form')
+const bCancel = document.getElementById('cancel_butt')
+const bLogin = document.getElementById('login_butt')
+const bShowPass = document.getElementById('show_pass_butt')
+const iPass = document.getElementById('pass_input')
+const iEmail = document.getElementById('email_input')
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function toggleSignForm(){
+    if(fSign.hidden){ 
+        // Показываем
+        fSign.hidden=false
+        // Добавляем и удаляем класс чтобы проигралась анимация 
+        //(sleep тоже нужен)
+        fSign.classList.add('sign_form_hidden')
+        await sleep(10);
+        fSign.classList.remove('sign_form_hidden')
+        console.log('unhidden r n')
+    }
+    else{ 
+        // Прячем
+        fSign.classList.add('sign_form_hidden')
+        await sleep(500); // Ждем 500 мс так как transition 0,5s
+        fSign.hidden=true
+    }
+}
+
+bCancel.addEventListener('click', toggleSignForm);
+bSign.addEventListener('click', toggleSignForm);
+
+bShowPass.addEventListener('pointerdown', ()=>{
+    iPass.setAttribute('type', 'text')
+    bShowPass.innerText = 'Скрыть пароль'
+});
+bShowPass.addEventListener('pointerup', ()=>{
+    iPass.setAttribute('type', 'password')
+    bShowPass.innerText = 'Показать пароль'
+});
+bLogin.addEventListener('click', (e)=>{
+    e.preventDefault();
+    validateForm();
+})
+
+function validateForm(){
+    iError.hidden = true;
+    
+    if(iPass.value.length < 6){
+        //Поставить текст об ошибке в iError
+        iError.hidden = false;
+        iError.innerText = 'Пароль должен быть не менее 6 символов';
+        return;
+    } 
+
+    //Проверить переменную iPass на наличие русских букв
+    if(iPass.value.match(/[а-я]/)){
+        iError.hidden = false;
+        iError.innerText = 'Пароль должен не должен содержать русские буквы';
+        return;
+    }
+    
+    //Проверить переменную iEmail на наличие адреса
+    if(!iEmail.value.match(/[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+        iError.hidden = false;
+        iError.innerText = 'Почта должен содержать адрес электронной почты';
+        return;
+    }
+
+    //Собрать данные с формы и в консоль
+    const data = {
+        email: iEmail.value,
+        password: iPass.value
+    }
+    console.log(data);
 }
